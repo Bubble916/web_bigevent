dbd42e2f97e35057307ef9305b8669223bd933a3
lf <1252145547@qq.com>
2021-08-20 21:46:17 +0800

    完成了首页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,16 @@
 $.ajaxPrefilter(function (options) {
   options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || '',
+    }
+  }
+  // 不论成功或失败，最终都会调用这个回调函数
+  // 统一挂载回调函数
+  options.complete = function (res) {
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+    }
+  }
 })
